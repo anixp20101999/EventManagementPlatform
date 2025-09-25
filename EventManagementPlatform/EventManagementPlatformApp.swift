ec7538b76f321fd026be1ec9ec1ed755a53121a3
anixp20101999 <animesh.rout@travelxp.tv>
2025-09-25 18:39:26 +0530

    Initial Commit

---
+++ EventManagementPlatform/EventManagementPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventManagementPlatformApp.swift
+//  EventManagementPlatform
+//
+//  Created by Animesh Rout on 25/09/25.
+//
+
+import SwiftUI
+
+@main
+struct EventManagementPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EventManagementPlatformTests/EventManagementPlatformTests.swift
@@ -0,0 +1,17 @@
+//
+//  EventManagementPlatformTests.swift
+//  EventManagementPlatformTests
+//
+//  Created by Animesh Rout on 25/09/25.
+//
+
+import Testing
+@testable import EventManagementPlatform
+
+struct EventManagementPlatformTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
